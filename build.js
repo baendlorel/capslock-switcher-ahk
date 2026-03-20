a12d264d3d8f521707e985b61c15259b50168bc1
aldia_mechdream <futami16237@gmail.com>
2026-03-21 00:21:24 +0800

    chore: 更新版本号并修改 .gitignore 和 build.js 文件

--- build.js
+++ build.js
@@ -5,58 +5,66 @@ import fs from 'fs';
 import path from 'path';
 
 const POSSIBLE_AHK_PATHS = [
-  ['Program Files', 'AutoHotkey', 'Compiler', 'AutoHotkey.exe'],
-  ['Program Files (x86)', 'utoHotkey', 'Compiler', 'AutoHotkey.exe'],
+  ['Program Files', 'AutoHotkey', 'Compiler', 'Ahk2Exe.exe'],
+  ['Program Files (x86)', 'utoHotkey', 'Compiler', 'Ahk2Exe.exe'],
 ];
 const POSSIBLE_DISK = [
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
+  'C:',
+  'D:',
+  'E:',
+  'F:',
+  'G:',
+  'H:',
+  'I:',
+  'J:',
+  'K:',
+  'L:',
+  'M:',
+  'N:',
+  'O:',
+  'P:',
+  'Q:',
+  'R:',
+  'S:',
+  'T:',
+  'U:',
+  'V:',
+  'W:',
+  'X:',
+  'Y:',
+  'Z:',
 ];
 
 const POSSIBLE_AHK_PATHS_WITH_DISK = POSSIBLE_DISK.reduce((prev, cur) => {
   const withDisk = POSSIBLE_AHK_PATHS.map((p) => [cur, ...p]);
   prev.push(...withDisk);
+  return prev;
 }, []).map((p) => path.join(...p));
 
 function getAhk() {
   const ahkPath = path.resolve('ahk-path.txt');
-  let ahk = '';
   if (fs.existsSync(ahkPath)) {
-    ahk = fs.readFileSync(ahkPath, 'utf-8').trim();
-  } else {
-    for (const possibleAhkPath of POSSIBLE_AHK_PATHS_WITH_DISK) {
-      if (fs.existsSync(possibleAhkPath)) {
-        ahk = possibleAhkPath;
-        fs.writeFileSync(ahkPath, ahk);
-        break;
-      }
+    const ahk = fs.readFileSync(ahkPath, 'utf-8').trim();
+    if (!ahk.includes('[Compiler Path Not Found]')) {
+      return ahk;
+    }
+  }
+
+  let ahk = null;
+  console.log(POSSIBLE_AHK_PATHS_WITH_DISK);
+  for (const possibleAhkPath of POSSIBLE_AHK_PATHS_WITH_DISK) {
+    if (fs.existsSync(possibleAhkPath)) {
+      ahk = possibleAhkPath;
+      fs.writeFileSync(ahkPath, ahk);
+      break;
     }
   }
 
   if (!ahk) {
-    fs.writeFileSync(ahkPath, 'Please put the absolute path to ...\\AutoHotkey\\Compiler\\Ahk2Exe.exe here');
+    fs.writeFileSync(
+      ahkPath,
+      '[Compiler Path Not Found] Please put the absolute path to ...\\AutoHotkey\\Compiler\\Ahk2Exe.exe here',
+    );
   }
 
   return ahk;
